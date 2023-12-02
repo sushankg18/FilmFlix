d66b046641352299510005522fc10d098ce94fde
Sushank Gautam <sushankg4@gmail.com>
2023-12-02 23:35:03 +0530

    added

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,31 +1,37 @@
-import { Container, Image, Text, HStack } from '@chakra-ui/react'
+import { Container, Image, Text, HStack, Input } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../Assets/MainLogo.png'
 import { Link } from 'react-router-dom'
+import { IoIosSearch } from "react-icons/io";
+
 
 const Navbar = () => {
     const styles = {
         textDecoration: "none",
-        cursor : "pointer",
-        color : "black",
+        cursor: "pointer",
+        color: "white",
     };
 
     return (
-        <Container bgColor={'#e5e5e5'} px={'12'}>
+        <Container bgColor={'#232323'} px={'20'} py={'20'}>
             <HStack justifyContent={'space-between'}>
                 <Link to={'/'}>
                     <Image cursor={'pointer'} src={logo} w={'120px'} />
                 </Link>
                 <HStack fontSize={'large'} fontFamily={'Outfit'} >
-                    <Link to={'/Bollywood'}style={{...styles , paddingLeft : '20px'}}>
+                    <Link to={'/Bollywood'} style={{ ...styles, paddingLeft: '20px' }}>
                         <Text>
                             Bollywood
                         </Text>
                     </Link>
-                    <Link to={'/Hollywood'} style={{...styles , paddingLeft : '20px'}} >Hollywood</Link>
-                    <Link to={'/Category'} style={{...styles , paddingLeft : '20px'}}>Category</Link>
-                    <Link to={'/topImdb'} style={{...styles , paddingLeft : '20px'}}>Top IMDB</Link>
-                    <Link to={'/animeMovies'} style={{...styles , paddingLeft : '20px'}}>Anime</Link>
+                    <Link to={'/Hollywood'} style={{ ...styles, paddingLeft: '20px' }} >Hollywood</Link>
+                    <Link to={'/Category'} style={{ ...styles, paddingLeft: '20px' }}>Category</Link>
+                    <Link to={'/topImdb'} style={{ ...styles, paddingLeft: '20px' }}>Top IMDB</Link>
+                    <Link to={'/animeMovies'} style={{ ...styles, paddingLeft: '20px' }}>Anime</Link>
+                </HStack>
+                <HStack background={'#444444'} width={'15rem'} p={'8'}>
+                    <Input placeholder='Search...' border={'none'} w={'100%'} background={'transparent'} />
+                    <IoIosSearch color='white' fontSize={'1.3rem'}/>
                 </HStack>
             </HStack>
         </Container>
